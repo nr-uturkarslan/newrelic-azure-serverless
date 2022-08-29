18afe838d7495a56a3dad47f6aa95a602107be34
uturkarslan <ugurturkarslan@gmail.com>
2022-08-29 19:45:53 +0300

    Add error handling

--- apps/DeviceService/DeviceService/Services/Create/CreateDeviceService.cs
+++ apps/DeviceService/DeviceService/Services/Create/CreateDeviceService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using DeviceService.Azure.CosmosDb;
+using DeviceService.Commons.Logging;
+using DeviceService.Controllers;
 using DeviceService.Dtos;
 using DeviceService.Entities;
 using DeviceService.Services.Create.Dtos;
@@ -34,32 +36,89 @@ public class CreateDeviceService : ICreateDeviceService
         CreateDeviceRequestDto requestDto
     )
     {
-        
-        var device = new Device
+        try
         {
-            Id = Guid.NewGuid().ToString(),
-            Name = requestDto.Name,
-            Description = requestDto.Description,
-            isValid = false,
-        };
+            LogCreatingDevice();
 
-        await _cosmosDbHandler.CreateItem(device);
+            var device = new Device
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = requestDto.Name,
+                Description = requestDto.Description,
+                isValid = false,
+            };
 
-        var responseDto = new CreateDeviceResponseDto
+            await _cosmosDbHandler.CreateItem(device);
+
+            var responseDto = new CreateDeviceResponseDto
+            {
+                Id = device.Id,
+                Name = device.Name,
+                Description = device.Description,
+            };
+
+            var response = new ResponseTemplate<CreateDeviceResponseDto>
+            {
+                Message = "Device is created successfully.",
+                StatusCode = HttpStatusCode.Created,
+                Data = responseDto,
+            };
+
+            LogDeviceCreatedSuccessfully();
+
+            return response;
+        }
+        catch (Exception e)
         {
-            Id = device.Id,
-            Name = device.Name,
-            Description = device.Description,
-        };
+            LogDeviceCreationFailed(e);
+            var response = new ResponseTemplate<CreateDeviceResponseDto>
+            {
+                Message = "Device creation is failed.",
+                StatusCode = HttpStatusCode.InternalServerError, 
+            };
 
-        var response = new ResponseTemplate<CreateDeviceResponseDto>
-        {
-            Message = "Device is created successfully.",
-            StatusCode = HttpStatusCode.Created,
-            Data = responseDto,
-        };
+            return response;
+        }
+    }
 
-        return response;
+    private void LogCreatingDevice()
+    {
+        CustomLogger.Run(_logger,
+            new CustomLog
+            {
+                ClassName = nameof(DeviceController),
+                MethodName = nameof(Run),
+                LogLevel = LogLevel.Information,
+                Message = "Creating device...",
+            });
+    }
+
+    private void LogDeviceCreatedSuccessfully()
+    {
+        CustomLogger.Run(_logger,
+            new CustomLog
+            {
+                ClassName = nameof(DeviceController),
+                MethodName = nameof(Run),
+                LogLevel = LogLevel.Information,
+                Message = "Device is created successfully.",
+            });
+    }
+
+    private void LogDeviceCreationFailed(
+        Exception e
+    )
+    {
+        CustomLogger.Run(_logger,
+            new CustomLog
+            {
+                ClassName = nameof(DeviceController),
+                MethodName = nameof(Run),
+                LogLevel = LogLevel.Error,
+                Message = "Device creation is failed.",
+                Exception = e.Message,
+                StackTrace = e.StackTrace,
+            });
     }
 }
 
