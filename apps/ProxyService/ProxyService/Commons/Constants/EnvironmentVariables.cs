6896aeaa4af7b5e1905e0b8efe4ed8ea8807718a
uturkarslan <ugurturkarslan@gmail.com>
2022-08-30 23:02:03 +0300

    Add OpenTelemetry

--- apps/ProxyService/ProxyService/Commons/Constants/EnvironmentVariables.cs
+++ apps/ProxyService/ProxyService/Commons/Constants/EnvironmentVariables.cs
@@ -3,6 +3,8 @@ namespace ProxyService.Commons.Constants;
 
 public static class EnvironmentVariables
 {
+    public static string NEW_RELIC_APP_NAME { get; set; }
+
     public static string DEVICE_SERVICE_URI_CREATE { get; set; }
 }
 
--- apps/ProxyService/ProxyService/Startup.cs
+++ apps/ProxyService/ProxyService/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using ProxyService.Services.Device.Create;
 using ProxyService.Commons.Constants;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 [assembly: FunctionsStartup(typeof(ProxyService.Startup))]
 
@@ -19,12 +22,57 @@ public class Startup : FunctionsStartup
 
         builder.Services.AddHttpClient();
 
+        // shared Resource to use for both OTel metrics AND tracing
+        var resource = ResourceBuilder.CreateDefault()
+            .AddService(EnvironmentVariables.NEW_RELIC_APP_NAME);
+
+        builder.Services.AddOpenTelemetryTracing(b =>
+        {
+            // use the OTLP exporter
+            b.AddOtlpExporter();
+
+            // receive traces from our own custom sources
+            b.AddSource(EnvironmentVariables.NEW_RELIC_APP_NAME);
+
+            // decorate our service name so we can find it when we search traces
+            b.SetResourceBuilder(resource);
+
+            // receive traces from built-in sources
+            b.AddHttpClientInstrumentation();
+            b.AddAspNetCoreInstrumentation();
+        });
+
+        builder.Services.AddOpenTelemetryMetrics(b =>
+        {
+            // add prometheus exporter
+            b.AddOtlpExporter();
+
+            // receive metrics from our own custom sources
+            b.AddMeter(EnvironmentVariables.NEW_RELIC_APP_NAME);
+
+            // decorate our service name so we can find it when we search metrics
+            b.SetResourceBuilder(resource);
+
+            // receive metrics from built-in sources
+            b.AddHttpClientInstrumentation();
+            b.AddAspNetCoreInstrumentation();
+        });
+
         builder.Services.AddSingleton<ICreateDeviceService, CreateDeviceService>();
+        
     }
 
     private void GetEnvironmentVariables()
     {
         Console.WriteLine("Getting environment variables...");
+
+        var newRelicAppName = Environment.GetEnvironmentVariable("NEW_RELIC_APP_NAME");
+        if (string.IsNullOrEmpty(newRelicAppName))
+        {
+            Console.WriteLine("[NEW_RELIC_APP_NAME] is not provided");
+            Environment.Exit(1);
+        }
+        EnvironmentVariables.NEW_RELIC_APP_NAME = newRelicAppName;
 
         var deviceServiceUri = Environment.GetEnvironmentVariable("DEVICE_SERVICE_URI");
         if (string.IsNullOrEmpty(deviceServiceUri))
