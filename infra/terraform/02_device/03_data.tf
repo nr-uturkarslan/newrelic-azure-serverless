3bebdd5e3ea249c8fef50a23390fe39c385f48f1
uturkarslan <ugurturkarslan@gmail.com>
2022-09-16 09:02:12 +0200

    Update Device

---
+++ infra/terraform/02_device/03_data.tf
@@ -0,0 +1,31 @@
+### Data ###
+
+# Resource Group - Platform
+data "azurerm_resource_group" "platform" {
+  name = var.resource_group_name_platform
+}
+
+# Cosmos DB Account - Platform
+data "azurerm_cosmosdb_account" "platform" {
+  name                = var.cosmos_db_account_name_platform
+  resource_group_name = var.resource_group_name_platform
+}
+
+# Cosmos DB - Device
+data "azurerm_cosmosdb_sql_database" "device" {
+  name                = var.cosmos_db_name_device
+  resource_group_name = var.resource_group_name_platform
+}
+
+# Service Bus - Platform
+data "azurerm_servicebus_namespace" "platform" {
+  name                = var.service_bus_namespace_name_platform
+  resource_group_name = var.resource_group_name_platform
+}
+
+# Service Bus Queue - Archive
+data "azurerm_servicebus_queue" "archive" {
+  name                = var.service_bus_queue_name_archive
+  resource_group_name = var.resource_group_name_platform
+  namespace_name      = var.service_bus_namespace_name_platform
+}
