e9b72285c1b6e1556115d7a143e52ad6bfc349ad
uturkarslan <ugurturkarslan@gmail.com>
2022-09-16 09:02:04 +0200

    Update Archive

--- infra/terraform/03_archive/05_app_service_archive.tf
+++ infra/terraform/03_archive/05_app_service_archive.tf
@@ -20,28 +20,36 @@ resource "azurerm_linux_web_app" "archive" {
 
   app_settings = {
 
+    # Container Registry
+    DOCKER_REGISTRY_SERVER_URL = data.azurerm_container_registry.platform.login_server
+    DOCKER_REGISTRY_SERVER_USERNAME = data.azurerm_container_registry.platform.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = data.azurerm_container_registry.platform.admin_password
+
     # Blob Container
     BLOB_CONTAINER_URI = "test"
 
     # Service Bus
-    SERVICE_BUS_FQDN       = "${azurerm_servicebus_namespace.platform.name}.servicebus.windows.net"
-    SERVICE_BUS_QUEUE_NAME = azurerm_servicebus_queue.archive.name
+    SERVICE_BUS_FQDN       = "${data.azurerm_servicebus_namespace.platform.name}.servicebus.windows.net"
+    SERVICE_BUS_QUEUE_NAME = data.azurerm_servicebus_queue.archive.name
 
-    # New Relic
-    NEW_RELIC_APP_NAME              = "ArchiveService"
-    NEW_RELIC_LICENSE_KEY           = var.new_relic_license_key
-    CORECLR_ENABLE_PROFILING        = "1"
-    CORECLR_PROFILER                = "{36032161-FFC0-4B61-B559-F6C5D41BAE5A}"
-    CORECLR_PROFILER_PATH           = "/home/site/wwwroot/newrelic/libNewRelicProfiler.so"
-    CORECLR_NEWRELIC_HOME           = "/home/site/wwwroot/newrelic"
-    NEWRELIC_PROFILER_LOG_DIRECTORY = "/home/LogFiles/NewRelic"
-
+    # # New Relic
+    # NEW_RELIC_APP_NAME              = "ArchiveService"
+    # NEW_RELIC_LICENSE_KEY           = var.new_relic_license_key
+    # CORECLR_ENABLE_PROFILING        = "1"
+    # CORECLR_PROFILER                = "{36032161-FFC0-4B61-B559-F6C5D41BAE5A}"
+    # CORECLR_PROFILER_PATH           = "/home/site/wwwroot/newrelic/libNewRelicProfiler.so"
+    # CORECLR_NEWRELIC_HOME           = "/home/site/wwwroot/newrelic"
+    # NEWRELIC_PROFILER_LOG_DIRECTORY = "/home/LogFiles/NewRelic"
   }
 
   site_config {
     application_stack {
-      docker_image     = "${var.container_registry_name_platform}.azurecr.io/archive"
-      docker_image_tag = "latest"
+      # docker_image     = "${data.azurerm_container_registry.platform.name}.azurecr.io/archive"
+      # docker_image_tag = "1663278324"
+      
+      # Random image for first deployment
+      docker_image     = "mcr.microsoft.com/oss/nginx/nginx"
+      docker_image_tag = "1.15.5-alpine"
     }
   }
 
@@ -54,6 +62,6 @@ resource "azurerm_linux_web_app" "archive" {
 resource "azurerm_role_assignment" "acr_pull_for_archive_as" {
   principal_id                     = azurerm_linux_web_app.archive.identity[0].principal_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.platform.id
+  scope                            = data.azurerm_container_registry.platform.id
   skip_service_principal_aad_check = true
 }
